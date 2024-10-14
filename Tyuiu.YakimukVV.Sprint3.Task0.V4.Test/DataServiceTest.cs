3c1e00fb56bcfd9385b74a0480ca1f9e93a83660
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-10-15 00:05:23 +0500

    Add project files.

---
+++ Tyuiu.YakimukVV.Sprint3.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.YakimukVV.Sprint3.Task0.V4.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyuiu.YakimukVV.Sprint3.Task0.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = new DataService();
+            int startValue = 1;
+            int stopValue = 7;
+
+            double result = service.GetMultiplySeries(startValue, stopValue);
+
+            Assert.AreEqual(1336.306, result, 0.001); 
+        }
+    }
+}
---
+++ Tyuiu.YakimukVV.Sprint3.Task0.V4/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.YakimukVV.Sprint3.Task0.V4.Lib;
+
+namespace Tyuiu.YakimukVV.Sprint3.Task0.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataService = new DataService();
+
+            int startValue = 1;
+            int stopValue = 7;
+
+            double result = dataService.GetMultiplySeries(startValue, stopValue);
+
+            Console.WriteLine($"Произведение ряда от {startValue} до {stopValue} равно: {result}");
+        }
+    }
+}
