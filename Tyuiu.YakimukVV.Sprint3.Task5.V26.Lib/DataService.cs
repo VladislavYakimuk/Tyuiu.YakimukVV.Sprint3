bde0f4d0fb5ed19be6d2fd16d13fc04582e14df5
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-10-16 14:13:56 +0500

    Task5

---
+++ Tyuiu.YakimukVV.Sprint3.Task5.V26.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+using System;
+
+namespace Tyuiu.YakimukVV.Sprint3.Task5.V26.Lib
+{
+    public class DataService : ISprint3Task5V26
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double result = 0;
+
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    result += Math.Pow(x, k) + Math.Sin(k);
+                }
+            }
+
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.YakimukVV.Sprint3.Task5.V26/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.YakimukVV.Sprint3.Task5.V26.Lib;
+
+namespace Tyuiu.YakimukVV.Sprint3.Task5.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 13;
+
+            double result = dataService.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
